e167561d69ebcb9c8698d76e58aff0c8c7f087c2
shaarkys <bretislav.kubesa@gmail.com>
2026-02-23 09:15:31 +0100

    Add SMART time/cloud read paths with legacy compatibility

--- src/shared/time.ts
+++ src/shared/time.ts
@@ -1,10 +1,48 @@
 import type { AnyDevice, SendOptions } from '../client';
+import { hasErrCode, isObjectLike } from '../utils';
 
 export default class Time {
   constructor(
     readonly device: AnyDevice,
     readonly apiModuleName: string,
   ) {}
+
+  private isSmartPath(sendOptions?: SendOptions): boolean {
+    return (
+      'shouldUseSmartMethods' in this.device &&
+      typeof this.device.shouldUseSmartMethods === 'function' &&
+      this.device.shouldUseSmartMethods(sendOptions)
+    );
+  }
+
+  private async ensureSupported(sendOptions?: SendOptions): Promise<void> {
+    if (
+      'negotiateSmartComponents' in this.device &&
+      typeof this.device.negotiateSmartComponents === 'function'
+    ) {
+      await this.device.negotiateSmartComponents(sendOptions);
+    }
+    if (
+      'hasComponent' in this.device &&
+      typeof this.device.hasComponent === 'function' &&
+      !this.device.hasComponent('time', undefined)
+    ) {
+      throw new Error('Time module is not supported for this device scope');
+    }
+  }
+
+  private toLegacyStyleTimePayload(response: unknown): unknown {
+    if (hasErrCode(response)) {
+      return response;
+    }
+    if (!isObjectLike(response)) {
+      return response;
+    }
+    return {
+      err_code: 0,
+      ...response,
+    };
+  }
 
   /**
    * Gets device's time.
@@ -15,6 +53,18 @@ export default class Time {
    * @throws {@link ResponseError}
    */
   async getTime(sendOptions?: SendOptions): Promise<unknown> {
+    if (this.isSmartPath(sendOptions)) {
+      await this.ensureSupported(sendOptions);
+      return this.toLegacyStyleTimePayload(
+        await this.device.sendSmartCommand(
+          'get_device_time',
+          undefined,
+          undefined,
+          sendOptions,
+        ),
+      );
+    }
+
     return this.device.sendCommand(
       {
         [this.apiModuleName]: { get_time: {} },
@@ -33,6 +83,18 @@ export default class Time {
    * @throws {@link ResponseError}
    */
   async getTimezone(sendOptions?: SendOptions): Promise<unknown> {
+    if (this.isSmartPath(sendOptions)) {
+      await this.ensureSupported(sendOptions);
+      return this.toLegacyStyleTimePayload(
+        await this.device.sendSmartCommand(
+          'get_device_time',
+          undefined,
+          undefined,
+          sendOptions,
+        ),
+      );
+    }
+
     return this.device.sendCommand(
       {
         [this.apiModuleName]: { get_timezone: {} },
